992c3e308463dcdd92534384dbfa9058c0b7bff9
onao13 <khovratovic15@yandex.ru>
2022-11-17 20:01:56 +0300

    Adding test controller for dev tests

--- backend/src/main/java/com/example/voiting/entity/Post.java
+++ backend/src/main/java/com/example/voiting/entity/Post.java
@@ -1,5 +1,6 @@
 package com.example.voiting.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,7 @@ public class Post {
 
     @NonNull
     @Column
+    @JsonIgnore
     private long userId;
 
     @NonNull
